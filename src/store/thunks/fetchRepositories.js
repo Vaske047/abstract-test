14b5a08df91982604196a86b2adfc472b7cf6acb
aleksandar <vasic.aleksandar.bg@gmail.com>
2023-05-05 21:41:43 +0200

    Update fetchRepositories

--- src/store/thunks/fetchRepositories.js
+++ src/store/thunks/fetchRepositories.js
@@ -8,14 +8,15 @@ const fetchRepositories = createAsyncThunk(
       `https://api.github.com/search/repositories`,
       {
         headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
+          'X-GitHub-Api-Version': '2022-11-28',
+          Authorization: 'Bearer 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'
         },
         params: {
-          q: params.query,
-          sort: params.sort,
-          order: params.order,
-          per_page: params.perPage,
-          page: params.page
+          q: params.query || '',
+          sort: params.sort || '',
+          order: params.order || '',
+          per_page: params.perPage || '',
+          page: params.page || ''
         }
       }
     )
