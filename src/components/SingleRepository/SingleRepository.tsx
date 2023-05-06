ec1cbff51907d89bf409037c764a0f91d4684678
aleksandar <vasic.aleksandar.bg@gmail.com>
2023-05-06 13:23:38 +0200

    Create single repo component

--- src/components/SingleRepository/SingleRepository.tsx
+++ src/components/SingleRepository/SingleRepository.tsx
@@ -1,6 +1,81 @@
-import { StyledSingleRepository } from './style'
+import { useEffect } from 'react'
+
+import { useSelector } from 'react-redux'
+
+import { Link } from 'react-router-dom'
+
+import { useLocation } from 'react-router-dom'
+
+import { fetchSingleRepository } from '../../store'
+import { useThunk } from '../../hooks/use-thunk'
+
+import {
+  StyledSingleRepository,
+  StyledRepoHeader,
+  StyledImgHolder,
+  StyledInfo,
+  StyledBackButton,
+  StyledRepoNav
+} from './style'
 const SingleRepository = () => {
-  return <StyledSingleRepository>Single Repository</StyledSingleRepository>
+  const location = useLocation()
+
+  const [
+    doFetchSingleRepository,
+    isLoadingRepositories,
+    loadingRepositoriesError
+  ] = useThunk(fetchSingleRepository)
+
+  const { data } = useSelector((state: any) => {
+    return state.singleRepository
+  })
+
+  useEffect(() => {
+    console.log(location.state)
+    if (typeof doFetchSingleRepository === 'function')
+      doFetchSingleRepository({
+        owner: location.state.owner,
+        name: location.state.name
+      })
+  }, [doFetchSingleRepository])
+
+  return (
+    <StyledSingleRepository>
+      <StyledBackButton>
+        <Link to='/'>Back to repositories</Link>
+      </StyledBackButton>
+      <StyledRepoHeader>
+        <StyledImgHolder>
+          <img src={data?.owner?.avatar_url} alt='avatar' />
+          <p>{data?.owner?.login}</p>
+        </StyledImgHolder>
+        <StyledInfo>
+          <h4>{data?.name}</h4>
+          <ul>
+            {data?.license && <li>{data?.license?.name}</li>}
+            <li>{data?.stargazers_count} stars</li>
+            <li>{data?.forks_count} forks</li>
+            <li>{data?.open_issues_count} open issues</li>
+          </ul>
+        </StyledInfo>
+      </StyledRepoHeader>
+      <StyledRepoNav>
+        <li>Contributor</li>
+        <li>Programming languages</li>
+      </StyledRepoNav>
+      {/* <p>contributors_url</p>
+      <p>languages_url</p> */}
+    </StyledSingleRepository>
+  )
 }
+// ● Repository name
+// ● Number of stars
+// ● Number of forks
+// ● Owner’s name
+// ● Owner’s avatar
+// ● Number of open issues
+// ● Contributor list (display first 10)
+// ● List of applied programming languages
+// ● ... any other information you find useful
 
 export default SingleRepository
--- src/components/SingleRepository/style.ts
+++ src/components/SingleRepository/style.ts
@@ -1,3 +1,83 @@
 import styled from 'styled-components'
 
-export const StyledSingleRepository = styled.div``
+export const StyledSingleRepository = styled.div`
+  max-width: 60vw;
+  margin: 40px auto;
+  padding: 40px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  border-radius: 6px;
+`
+
+export const StyledRepoHeader = styled.div`
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 40px;
+`
+
+export const StyledImgHolder = styled.div`
+  img {
+    max-width: 200px;
+    border-radius: 50%;
+  }
+  p {
+    margin: 20px 0 0;
+    font-size: 24px;
+    font-weight: 600px;
+    text-align: center;
+  }
+`
+
+export const StyledInfo = styled.div`
+  h4 {
+    font-size: 24px;
+    margin-bottom: 20px;
+    margin-top: 0;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    list-style: none;
+    padding: 0;
+    li {
+      background-color: #081159;
+      color: #fff;
+      font-size: 12px;
+      padding: 5px 10px;
+      border-radius: 6px;
+    }
+  }
+`
+
+export const StyledBackButton = styled.div`
+  display: flex;
+  a {
+    padding: 10px;
+    background-color: #081159;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 6px;
+  }
+`
+export const StyledRepoNav = styled.ul`
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  padding: 10px 0;
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
+  li {
+    paddiing: 10px;
+    text-align: center;
+    padding: 20px;
+    cursor: pointer;
+    color: #a1a1a1;
+    &:hover {
+      box-shadow: inset 0 -2px 0 0 #081159;
+      color: #081159;
+    }
+  }
+`
