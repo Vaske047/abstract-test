0e709a94422b71bf490fa07e94022e8929a51d93
aleksandar <vasic.aleksandar.bg@gmail.com>
2023-05-06 13:23:08 +0200

    Add fetch single repo to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { repositoriesReducer } from './slices/repositoriesSlice'
+import { singleRepositoryReducer } from './slices/singleRepositorySlice'
 
 export const store = configureStore({
   reducer: {
-    repositories: repositoriesReducer
+    repositories: repositoriesReducer,
+    singleRepository: singleRepositoryReducer
   }
 })
 
 export * from './thunks/fetchRepositories'
+export * from './thunks/fetchSingleRepository'
